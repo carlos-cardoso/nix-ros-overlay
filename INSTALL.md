9a0b3ce4485461024c1adc1209d75dda26559014
carlos <carlos.cardoso@tecnico.ulisboa.pt>
2021-07-09 08:12:40 +0100

    Scripts for loading kinect in nix-ros

---
+++ INSTALL.md
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Install Nix: https://nixos.org/nix/download.html
+# bash <(curl -L https://nixos.org/nix/install)
+#
+# (Optional) configure Nix to use ROS Cachix binary cache
+# nix-env -iA cachix -f https://cachix.org/api/v1/install
+# cachix use ros
+#
+# git clone https://github.com/lopsided98/nix-ros-overlay.git
+# cd nix-ros-overlay
+# git checkout 54fb4baeb206eead92ba68a9a593c3607c1c4f8c
+
+# sh load_kinect_shell.sh
+# mkdir catkin_ws && cd catkin_ws
+# catkin_init_workspace
+# mkdir src && cd src
+# git clone https://github.com/OpenKinect/libfreenect2.git
+# cd libfreenect2
+# git checkout fd64c5d9b214df6f6a55b4419357e51083f15d93
+#
+# mkdir build && cd build && cmake .. && make && sudo make install
+# cd ../..
+#
+# git clone https://github.com/ros-perception/image_transport_plugins
+# cd image_transport_plugins
+# git checkout 0162f74e389d139b571d395c0bbb3c8b9d013a94
+# cd ..
+#
+# git clone https://github.com/TheEngineRoom-UniGe/iai_kinect2.git
+# cd iai_kinect2
+# git checkout 09612539245053bd99b48cf5dda0b951f8e6bb39
+# cd ..
+#
+# catkin_make
+# source devel/setup.bash
+# rosrun kinect_bridge kinect_bridge
+# rosrun kinect_viewer kinect_viewer
---
+++ examples/libfreenect2_ros_bridge.nix
@@ -0,0 +1,66 @@
+# Run:
+# roslaunch turtlebot3_gazebo turtlebot3_world.launch
+# roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch
+
+with import ../. {};
+with rosPackages.noetic;
+with pythonPackages;
+
+
+let
+   libusb_1_0_20 = pkgs.stdenv.mkDerivation rec {
+     name = "libusb-1.0.20";
+
+     src = pkgs.fetchurl {
+       url = "mirror://sourceforge/libusb/${name}.tar.bz2";
+       sha256 = "1zzp6hc7r7m3gl6zjbmzn92zkih4664cckaf49l1g5hapa8721fb";
+     };
+
+     outputs = [ "out" "dev" ]; # get rid of propagating systemd closure
+
+     buildInputs = [ pkgs.pkgconfig ];
+     propagatedBuildInputs =
+       pkgs.lib.optional pkgs.stdenv.isLinux pkgs.systemd ++
+       pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.libobjc pkgs.IOKit ];
+
+     NIX_LDFLAGS = pkgs.lib.optionalString pkgs.stdenv.isLinux "-lgcc_s";
+
+     preFixup = pkgs.lib.optionalString pkgs.stdenv.isLinux ''
+     sed 's,-ludev,-L${pkgs.systemd}/lib -ludev,' -i $out/lib/libusb-1.0.la
+     '';
+};
+in
+
+mkShell {
+  buildInputs = [
+    pkgs.bashInteractive
+    pkgs.libva
+    #pkgs.cudatoolkit_11_2
+    pkgs.libjpeg_turbo
+    pkgs.glfw3
+    pkgs.opencv4
+    libusb_1_0_20
+    pkgs.freeglut
+    pkgs.libGLU
+    pkgs.libGL
+    glibcLocales
+    libogg
+    libtheora
+    libusb1
+    (buildEnv { paths = [
+      rosbash
+      turtlebot3-description
+      turtlebot3-teleop
+      turtlebot3-gazebo
+      gazebo-plugins
+      xacro
+      perception-pcl
+    ]; })
+  ]  ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.GLUT pkgs.Cocoa ];
+
+  nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config  ];
+
+  ROS_HOSTNAME = "localhost";
+  ROS_MASTER_URI = "http://localhost:11311";
+  TURTLEBOT3_MODEL = "burger";
+}
